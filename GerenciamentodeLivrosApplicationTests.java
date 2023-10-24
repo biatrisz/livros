93b3430a75f0d87d4adbf8d7a2e7b082cc23ca33
biatrisz <132359853+biatrisz@users.noreply.github.com>
2023-10-24 09:25:57 -0300

    Add files via upload

---
+++ GerenciamentodeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GerenciamentodeLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentodeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
